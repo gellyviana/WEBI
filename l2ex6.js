295feca5b76ee084b4e013ad399d3b012ecc853a
gellyviana <gellyvianamota1988@gmail.com>
2017-09-19 17:00:44 -0300

    Concertado adição de linhas na tabela dinamicamente

--- l2ex6.js
+++ l2ex6.js
@@ -1,19 +1,21 @@
 function deletaRow(){
-	document.getElementById("myTable").deleteRow(1); 
+	document.getElementById("myTable").deleteRow(); 
 }
 
 function confirma(){
 	var info = document.getElementById("form");
 	var table = document.getElementById("myTable");
-	var row = table.insertRow(1);
-	var cell1 = row.insertCell(0);
-	var cell2 = row.insertCell(1);
-	var cell3 = row.insertCell(2);
-	var cell4 = row.insertCell(3);
+	var row = table.insertRow();
+	var cell1 = row.insertCell();
+	var cell2 = row.insertCell();
+	var cell3 = row.insertCell();
+	var cell4 = row.insertCell();
+	var cell5 = row.insertCell();
 
 	cell1.innerHTML = info.elements[0].value;
 	cell2.innerHTML = info.elements[1].value;
 	cell3.innerHTML = info.elements[2].value;
 	cell4.innerHTML = info.elements[3].value;
+	cell5.innerHTML = '<button onclick="deletaRow()">Excluir</button>';
 
 }
